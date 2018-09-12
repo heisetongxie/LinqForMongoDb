0d5c9b2b5773b5191c9e7f88f24f9eb4f707cfdb
吴春林 <weiainiandy@hotmail.com>
2018-09-12 10:20:20 +0800

    添加项目文件。

---
+++ LinqForMongodb/Dbset.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace LinqForMongodb
+{
+    public class Dbset<T> :IQueryable<T>, IQueryable
+    {
+
+        public Dbset()
+        {
+            this.Expression = Expression.Constant(this);
+            this.Provider = new MongoDbProvider<T>();
+        }
+
+        public Dbset(Expression expression, IQueryProvider provider)
+        {
+            this.Expression = expression;
+            this.Provider = provider;
+        }
+
+        public Type ElementType
+        {
+            get
+            {
+                 return typeof(Dbset<T>);
+            }
+        }
+
+        public Expression Expression { get; set; }
+
+        public IQueryProvider Provider { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            var result = Provider.Execute<List<T>>(Expression);
+            if (result == null)
+                yield break;
+            foreach (var item in result)
+            {
+                yield return item;
+            }
+        }
+
+
+        //public T Add(T TEntity)
+        //{
+
+        //}
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return Provider.Execute<IEnumerator<T>>(Expression);
+        }
+    }
+}
---
+++ LinqForMongodb/MongodbContext.cs
@@ -0,0 +1,79 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqForMongodb.Context
+{
+    /// <summary>
+    /// Mongodb数据库连接上下文
+    /// </summary>
+    public class MongodbContext:IDisposable
+    {
+        /// <summary>
+        /// 实例化Mongodb数据库连接
+        /// </summary>
+        /// <param name="conn">连接字符串</param>
+        public MongodbContext(string conn)
+        {
+            this.ConnectString = conn;
+            this.mongoClient = new MongoClient(this.ConnectString);
+        }
+
+        /// <summary>
+        /// MongoDb数据库客户端
+        /// </summary>
+        public MongoClient mongoClient { get; set; }
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public string ConnectString { get; set; }
+
+        public IQueryable<T> Table<T>()
+        {
+            return new Dbset<T>();
+        }
+        
+        #region IDisposable Support
+        private bool disposedValue = false; // 要检测冗余调用
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)。
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
+                // TODO: 将大型字段设置为 null。
+                this.mongoClient = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
+        // ~MongodbContext() {
+        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+        //   Dispose(false);
+        // }
+
+        // 添加此代码以正确实现可处置模式。
+        public void Dispose()
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose(true);
+            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
+            // GC.SuppressFinalize(this);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
